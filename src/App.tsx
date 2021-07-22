c93b61e56322a519e1f1e0fc6beff39a923cb052
nick-landry-it <nicklandryit@gmail.com>
2021-07-22 18:37:48 -0500

    Added styling with state change using mockup data

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,32 @@ const DisplayStars = (props: any) => (
 )
 
 const PlayNumber = (props: any) => (
-    <button className="number" onClick={() => console.log('Num', props.number)}>
+    <button
+        className="number"
+        style={{ backgroundColor: colors[props.status] }}
+        onClick={() => console.log('Num', props.number)}
+    >
         {props.number}
     </button>
 )
 
 const StarMatch = () => {
     const [stars, setStars] = useState(utils.random(1, 9))
+    const [availableNums, setAvailableNums] = useState([1, 2, 3, 4, 5])
+    const [candidateNums, setCandidateNums] = useState([2, 3])
+
+    const candidatesAreWrong = utils.sum(candidateNums) > stars
+
+    const numberStatus = (number: any) => {
+        if (!availableNums.includes(number)) {
+            return 'used'
+        }
+        if (candidateNums.includes(number)) {
+            return candidatesAreWrong ? 'wrong' : 'candidate'
+        }
+        return 'available'
+    }
+
     return (
         <div className="game">
             <div className="help">
@@ -29,7 +48,11 @@ const StarMatch = () => {
                 </div>
                 <div className="right">
                     {utils.range(1, 9).map((number) => (
-                        <PlayNumber key={number} number={number} />
+                        <PlayNumber
+                            key={number}
+                            status={numberStatus(number)}
+                            number={number}
+                        />
                     ))}
                 </div>
             </div>
@@ -39,7 +62,7 @@ const StarMatch = () => {
 }
 
 // Color Theme
-const colors = {
+const colors: any = {
     available: 'lightgray',
     used: 'lightgreen',
     wrong: 'lightcoral',
