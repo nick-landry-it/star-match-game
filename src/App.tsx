5005699c1a29a0a472293f87107b7489b8975995
nick-landry-it <nicklandryit@gmail.com>
2021-07-22 17:07:23 -0500

    Prettier write

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,81 @@
 // STAR MATCH - Starting Template
 
-import { useState } from 'react';
+import { useState } from 'react'
 
 const DisplayStars = (props: any) => (
-  <>
-    {utils.range(1, props.starCount).map((starId) => (
-      <div key={starId} className="star" />
-    ))}
-  </>
-);
+    <>
+        {utils.range(1, props.starCount).map((starId) => (
+            <div key={starId} className="star" />
+        ))}
+    </>
+)
 
 const PlayNumber = (props: any) => (
-  <button className="number" onClick={() => console.log('Num', props.number)}>
-    {props.number}
-  </button>
-);
+    <button className="number" onClick={() => console.log('Num', props.number)}>
+        {props.number}
+    </button>
+)
 
 const StarMatch = () => {
-  const [stars, setStars] = useState(utils.random(1, 9));
-  return (
-    <div className="game">
-      <div className="help">
-        Pick 1 or more numbers that sum to the number of stars
-      </div>
-      <div className="body">
-        <div className="left">
-          <DisplayStars starCount={stars} />
+    const [stars, setStars] = useState(utils.random(1, 9))
+    return (
+        <div className="game">
+            <div className="help">
+                Pick 1 or more numbers that sum to the number of stars
+            </div>
+            <div className="body">
+                <div className="left">
+                    <DisplayStars starCount={stars} />
+                </div>
+                <div className="right">
+                    {utils.range(1, 9).map((number) => (
+                        <PlayNumber key={number} number={number} />
+                    ))}
+                </div>
+            </div>
+            <div className="timer">Time Remaining: 10</div>
         </div>
-        <div className="right">
-          {utils.range(1, 9).map((number) => (
-            <PlayNumber key={number} number={number} />
-          ))}
-        </div>
-      </div>
-      <div className="timer">Time Remaining: 10</div>
-    </div>
-  );
-};
+    )
+}
 
 // Color Theme
 const colors = {
-  available: 'lightgray',
-  used: 'lightgreen',
-  wrong: 'lightcoral',
-  candidate: 'deepskyblue',
-};
+    available: 'lightgray',
+    used: 'lightgreen',
+    wrong: 'lightcoral',
+    candidate: 'deepskyblue',
+}
 
 // Math science
 const utils = {
-  // Sum an array
-  sum: (arr: any[]) => arr.reduce((acc: any, curr: any) => acc + curr, 0),
+    // Sum an array
+    sum: (arr: any[]) => arr.reduce((acc: any, curr: any) => acc + curr, 0),
 
-  // create an array of numbers between min and max (edges included)
-  range: (min: number, max: number) =>
-    Array.from({ length: max - min + 1 }, (_, i) => min + i),
+    // create an array of numbers between min and max (edges included)
+    range: (min: number, max: number) =>
+        Array.from({ length: max - min + 1 }, (_, i) => min + i),
 
-  // pick a random number between min and max (edges included)
-  random: (min: number, max: number) =>
-    min + Math.floor(Math.random() * (max - min + 1)),
+    // pick a random number between min and max (edges included)
+    random: (min: number, max: number) =>
+        min + Math.floor(Math.random() * (max - min + 1)),
 
-  // Given an array of numbers and a max...
-  // Pick a random sum (< max) from the set of all available sums in arr
-  randomSumIn: (arr: string | any[], max: number) => {
-    const sets = [[]];
-    const sums = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0, len = sets.length; j < len; j++) {
-        const candidateSet = sets[j].concat(arr[i]);
-        const candidateSum = utils.sum(candidateSet);
-        if (candidateSum <= max) {
-          sets.push(candidateSet);
-          sums.push(candidateSum);
+    // Given an array of numbers and a max...
+    // Pick a random sum (< max) from the set of all available sums in arr
+    randomSumIn: (arr: string | any[], max: number) => {
+        const sets = [[]]
+        const sums = []
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0, len = sets.length; j < len; j++) {
+                const candidateSet = sets[j].concat(arr[i])
+                const candidateSum = utils.sum(candidateSet)
+                if (candidateSum <= max) {
+                    sets.push(candidateSet)
+                    sums.push(candidateSum)
+                }
+            }
         }
-      }
-    }
-    return sums[utils.random(0, sums.length - 1)];
-  },
-};
+        return sums[utils.random(0, sums.length - 1)]
+    },
+}
 
-export default StarMatch;
+export default StarMatch
